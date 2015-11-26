e81f30d86ed999060b934a1a89a66a12a5673764
Kim Jami <kiim.jami@gmail.com>
2015-11-26 17:26:06 +0100

    Initial setup

---
+++ src/simple-image-slider.js
@@ -0,0 +1,17 @@
+
+/*
+ * Simple Image Slider 0.1
+ * https://github.com/clinteastwo0d/simple-image-slider
+ *
+ * Copyright 2015, Kim Jami
+ * Free to use and abuse under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+;(function($) {
+
+   $.fn.leanSlider = function(options) {
+      // code
+   }
+
+})(jQuery);
